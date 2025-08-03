c845f8dd8ddbafdf5bcd9775e6adfc261bd20cec
Marius048 <grandthaft5@gmx.de>
2025-08-03 21:00:42 +0200

    Update login.js

--- login.js
+++ login.js
@@ -7,3 +7,18 @@ netlifyIdentity.on('login', user => {
 netlifyIdentity.on('logout', () => {
   document.getElementById('adminContent').style.display = 'none';
 });
+
+const urlParams = new URLSearchParams(window.location.search);
+const inviteToken = urlParams.get("token");
+
+if (inviteToken) {
+  const password = prompt("Bitte neues Passwort setzen:");
+  netlifyIdentity.acceptInvite(inviteToken, password)
+    .then(user => {
+      console.log("Einladung angenommen!", user);
+      document.getElementById('adminContent').style.display = 'block';
+    })
+    .catch(error => {
+      console.error("Fehler beim Setzen des Passworts:", error);
+    });
+}

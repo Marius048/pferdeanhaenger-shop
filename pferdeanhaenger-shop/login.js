33c45b90485bbeacd022d7c6c9a146e5c2f1916c
Marius048 <grandthaft5@gmx.de>
2025-08-03 20:52:31 +0200

    Delete pferdeanhaenger-shop directory

--- pferdeanhaenger-shop/login.js
+++
@@ -1,9 +0,0 @@
-netlifyIdentity.init();
-
-netlifyIdentity.on('login', user => {
-  document.getElementById('adminContent').style.display = 'block';
-});
-
-netlifyIdentity.on('logout', () => {
-  document.getElementById('adminContent').style.display = 'none';
-});
